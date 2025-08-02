f4c56917a0397900605f6dff4f4e96d81ecb4a0d
Pablo George <pablogeokar@hotmail.com>
2025-08-02 08:16:14 -0300

    terminando o model de partidaContabil

--- lib/models/partida_contabil.dart
+++ lib/models/partida_contabil.dart
@@ -15,5 +15,48 @@ class PartidaContabil {
     DateTime? dataCriacao,
   }) : dataCriacao = dataCriacao ?? DateTime.now();
 
-  // toMap, fromMap, copyWith e toString similares aos modelos anteriores
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'lancamentoId': lancamentoId,
+      'contaId': contaId,
+      'tipo': tipo,
+      'valor': valor,
+      'dataCriacao': dataCriacao.toIso8601String(),
+    };
+  }
+
+  factory PartidaContabil.fromMap(Map<String, dynamic> map) {
+    return PartidaContabil(
+      id: map['id'],
+      lancamentoId: map['lancamentoId'],
+      contaId: map['contaId'],
+      tipo: map['tipo'],
+      valor: map['valor'],
+      dataCriacao: DateTime.parse(map['data_criacao']),
+    );
+  }
+
+  PartidaContabil copyWith({
+    int? id,
+    int? lancamentoId,
+    int? contaId,
+    String? tipo, // 'DEBITO' ou 'CREDITO'
+    double? valor,
+    DateTime? dataCriacao,
+  }) {
+    return PartidaContabil(
+      id: id ?? this.id,
+      lancamentoId: lancamentoId ?? this.lancamentoId,
+      contaId: contaId ?? this.contaId,
+      tipo: tipo ?? this.tipo,
+      valor: valor ?? this.valor,
+      dataCriacao: dataCriacao ?? this.dataCriacao,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'PartidaContabil(id: $id, lancamentoId: $lancamentoId, contaID: $contaId, tipo: $tipo, valor: $valor, dataCriacao: $dataCriacao)';
+  }
 }
